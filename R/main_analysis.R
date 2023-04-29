cc9b4d02ea61b358f055e0281ec5eb14c68baf1b
Matías Castillo <matcasti@umag.cl>
2023-04-29 16:51:38 -0300

    correlations and report

--- R/main_analysis.R
+++ R/main_analysis.R
@@ -1,4 +1,15 @@
 library(data.table)
 data("cyclist")
 
+cyclist[, sapply(cyclist, is.numeric), with = FALSE] |>
+  vapply(function(i) shapiro.test(i)$p.value > 0.05, FUN.VALUE = 1) |>
+  table()
 
+
+corr <- correlation::correlation(cyclist[, sapply(cyclist, is.numeric), with = FALSE], p_adjust = "none", method = "spearman")
+
+dplyr::filter(.data = corr, p < 0.05,
+              !((Parameter1 %like% "^emg") & (Parameter2 %like% "^emg")),
+              !((Parameter1 %like% "^jump_") & (Parameter2 %like% "^jump_")),
+              !((Parameter1 %like% "^iso_") & (Parameter2 %like% "^iso_"))) |>
+  dplyr::arrange(rho)
