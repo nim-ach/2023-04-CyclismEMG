ce2b0c462de0a9c0ead562be68336ea7bd72fc85
Matías Castillo <matcasti@umag.cl>
2023-05-26 18:41:46 -0300

    many changes on model fitting and plotting
    - separate files for model fitting and model plotting
    - mainly for manuscript writing purposes (when the time comes)

--- R/bda/make_models.R
+++ R/bda/make_models.R
@@ -12,21 +12,19 @@ library(brms) ## Paquete principal
 ## Cargamos los datos
 data("cyclist")
 
-
 ########################################################
 ############## Estimación con iso_quad #################
 ########################################################
 
 # Ajustamos los modelos ---------------------------------------------------
 
-quad_iso_data <- cyclist[!is.na(iso_mean_torque_quad_der_raw), list(
-  mean_torque_quad = mean(x = c(iso_mean_torque_quad_der_raw, iso_mean_torque_quad_izq_raw)),
-  sd_torque_quad = mean(x = c(iso_sd_torque_quad_der_raw, iso_sd_torque_quad_izq_raw)),
-  emg_mean_1
-  ), sujetos]
-
-## Vemos los priors por defecto
-get_prior(formula = mean_torque_quad | se(sd_torque_quad) ~ emg_mean_1, data = quad_iso_data)
+quad_data <- cyclist[i = !is.na(iso_mean_torque_quad_der_raw),
+                     j = list(
+                       mean_torque_quad = mean(x = c(iso_mean_torque_quad_der_raw, iso_mean_torque_quad_izq_raw)),
+                       sd_torque_quad = mean(x = c(iso_sd_torque_quad_der_raw, iso_sd_torque_quad_izq_raw)),
+                       emg_mean_1
+                     ),
+                     by = sujetos]
 
 quad_model <- brm(
   ## Especificamos el modelo
@@ -59,108 +57,6 @@ pp_check(quad_model, type = "loo_ribbon")
 pp_check(quad_model, type = "stat_freqpoly", ndraws = 1e4, binwidth = 2)
 
 
-# Visualización de los resultados -----------------------------------------
-
-## Primer gráfico -----
-
-## Simulación de predictores a intervalos regulares
-sim_emg <- seq(from = min(cyclist$emg_mean_1),
-               to = max(cyclist$emg_mean_1),
-               length.out = 10)
-
-## Simulación de predictores a intervalos regulares
-sim_emg <- data.frame(emg_mean_1 = seq(from = min(cyclist$emg_mean_1),
-                                       to = max(cyclist$emg_mean_1),
-                                       length.out = 10),
-                      sd_torque_quad = median(quad_iso_data$sd_torque_quad))
-
-
-## Usamos el modelos con los predictores simulados
-pred_data <- predict(object = model,
-                     newdata = sim_emg,
-                     probs = c(0.025, .1, .25, .75, .9, .975),
-                     robust = TRUE,
-                     cores = 5) |>
-  cbind(sim_emg) |>
-  as.data.table()
-
-## Generamos el primer gráfico
-p1 <- ggplot(pred_data, aes(emg_mean_1, Estimate)) +
-  geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5, fill = "95%")) +
-  geom_ribbon(aes(ymin = Q10, ymax = Q90, fill = "80%")) +
-  geom_ribbon(aes(ymin = Q25, ymax = Q75, fill = "50%")) +
-  scale_fill_manual(values = `names<-`(RColorBrewer::brewer.pal(3, "Blues"), c("95%", "80%", "50%"))) +
-  geom_line() +
-  geom_point(data = quad_iso_data, aes(emg_mean_1, mean_torque_quad)) +
-  scale_y_continuous(n.breaks = 6, limits = c(0, NA)) +
-  scale_x_continuous(limits = c(0, NA)) +
-  labs(x = "Mean EMG", y = "Mean quadriceps torque", fill = "CI") +
-  see::theme_modern()
-
-print(p1)
-
-## Segundo gráfico -----
-
-## Obtenemos los muestreos de las simulaciones de las Cadenas de Markov
-post_dist <- tidy_draws(model) |>
-  as.data.table()
-
-## Calculamos la media acumulada por cadena para ilustrar la convergencia a la
-## solución del modelo
-post_dist[j = cumulative_emg_mean_1 := cumsum(b_emg_mean_1) / seq_along(b_emg_mean_1),
-          by = list(.chain)]
-
-ribbon_data <- posterior_summary(model, variable = "b_emg_mean_1")
-
-## Generamos el segundo gráfico
-p2 <- ggplot(post_dist, aes(.iteration, b_emg_mean_1, col = as.factor(.chain))) +
-  geom_line() +
-  labs(x = "Iterations",
-       y = "Estimated linear effect of\nEMG on quadriceps Torque",
-       col = "Chain") +
-  geom_ysidehistogram(aes(fill = as.factor(.chain)), bins = 100,
-                      show.legend = FALSE) +
-  geom_hline(yintercept = ribbon_data[,"Estimate"]) +
-  geom_hline(yintercept = ribbon_data[,c("Q2.5","Q97.5")], lty = 2) +
-  scale_x_continuous(expand = c(0,0)) +
-  scale_y_continuous(n.breaks = 6) +
-  scale_ysidex_continuous(expand = c(0,0), breaks = NULL, labels = NULL) +
-  scale_color_brewer(aesthetics = c("colour", "fill")) +
-  see::theme_modern() +
-  theme(ggside.panel.scale = 1/3)
-
-print(p2)
-
-## Tercer gráfico ----
-
-## Preparamos los datos a usar
-plot_chains_data <- post_dist[, list(
-    emg_mean = cumulative_emg_mean_1,
-    iter = .iteration,
-    chain = as.factor(.chain)
-  )]
-
-## Estimación del valor central del parámetro modelado
-estimated_value <- bayestestR::point_estimate(model, centrality = "median", parameters = "emg")[[2]]
-
-p3 <- ggplot(plot_chains_data, aes(iter, emg_mean)) +
-  geom_line(aes(col = chain)) +
-  labs(x = "Iterations", y = "Estimated linear effect of\nEMG on quadriceps Torque",
-       col = "Chain") +
-  scale_color_brewer() +
-  scale_x_log10() +
-  geom_hline(yintercept = estimated_value, lty = 2) +
-  see::theme_modern()
-
-print(p3)
-
-## Unimos los gráficos en uno solo ----
-
-p_plots <- (guide_area() / ((p1 + p3) / p2)) +
-  plot_layout(guides = "collect", heights = c(1,9)) &
-  theme(legend.position = "top")
-
-
 ########################################################
 ############# Estimación con iso_isquio ################
 ########################################################
@@ -168,11 +64,13 @@ p_plots <- (guide_area() / ((p1 + p3) / p2)) +
 
 # Ajustamos los modelos ---------------------------------------------------
 
-isquio_iso_data <- cyclist[!is.na(iso_mean_torque_isquio_der_raw), list(
-  mean_torque_isquio = mean(x = c(iso_mean_torque_isquio_der_raw, iso_mean_torque_isquio_izq_raw)),
-  sd_torque_isquio = mean(x = c(iso_sd_torque_isquio_der_raw, iso_sd_torque_isquio_izq_raw)),
-  emg_mean_1
-), sujetos]
+isqt_data <- cyclist[i = !is.na(iso_mean_torque_isquio_der_raw),
+                     j = list(
+                       mean_torque_isquio = mean(x = c(iso_mean_torque_isquio_der_raw, iso_mean_torque_isquio_izq_raw)),
+                       sd_torque_isquio = mean(x = c(iso_sd_torque_isquio_der_raw, iso_sd_torque_isquio_izq_raw)),
+                       emg_mean_1
+                     ),
+                     by = sujetos]
 
 ## Vemos los priors por defecto
 get_prior(formula = mean_torque_isquio | se(sd_torque_isquio) ~ emg_mean_1, data = isquio_iso_data)
@@ -200,117 +98,9 @@ model_2 <- brm(
 saveRDS(model_2, file = "R/Bayesian models/models/isquios_torque_&_emg.RDS")
 
 ## Valoración del ajuste del modelo
-# pp_check(model_2, ndraws = 200)  # shows dens_overlay plot by default
-# pp_check(model_2, type = "error_hist", ndraws = 12)
-# pp_check(model_2, type = "scatter_avg")
-# pp_check(model_2, type = "stat_2d")
-# pp_check(model_2, type = "loo_ribbon")
-# pp_check(model_2, type = "stat_freqpoly", ndraws = 1e4, binwidth = 2)
-
-
-# Visualización de los resultados -----------------------------------------
-
-## Primer gráfico -----
-
-## Simulación de predictores a intervalos regulares
-sim_emg <- data.frame(emg_mean_1 = seq(from = min(cyclist$emg_mean_1),
-                                       to = max(cyclist$emg_mean_1),
-                                       length.out = 10),
-                      sd_torque_isquio = median(isquio_iso_data$sd_torque_isquio))
-
-## Usamos el modelos con los predictores simulados
-pred_data <- predict(object = model_2,
-                     newdata = sim_emg,
-                     probs = c(0.025, .1, .25, .75, .9, .975),
-                     robust = TRUE,
-                     cores = 5) |>
-  cbind(sim_emg) |>
-  as.data.table()
-
-## Generamos el primer gráfico
-q1 <- ggplot(pred_data, aes(emg_mean_1, Estimate)) +
-  geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5, fill = "95%")) +
-  geom_ribbon(aes(ymin = Q10, ymax = Q90, fill = "80%")) +
-  geom_ribbon(aes(ymin = Q25, ymax = Q75, fill = "50%")) +
-  scale_fill_manual(values = `names<-`(RColorBrewer::brewer.pal(3, "Blues"), c("95%", "80%", "50%"))) +
-  geom_line() +
-  geom_point(data = isquio_iso_data, aes(emg_mean_1, mean_torque_isquio)) +
-  scale_y_continuous(n.breaks = 6, limits = c(0, NA)) +
-  scale_x_continuous(limits = c(0, NA)) +
-  labs(x = "Mean EMG", y = "Mean Hamstring Torque", fill = "CI") +
-  see::theme_modern()
-
-print(q1)
-
-## Segundo gráfico -----
-
-## Obtenemos los muestreos de las simulaciones de las Cadenas de Markov
-post_dist <- tidy_draws(model_2) |>
-  as.data.table()
-
-## Calculamos la media acumulada por cadena para ilustrar la convergencia a la
-## solución del modelo
-post_dist[j = cumulative_emg_mean_1 := cumsum(b_emg_mean_1) / seq_along(b_emg_mean_1),
-          by = list(.chain)]
-
-ribbon_data <- posterior_summary(model_2, variable = "b_emg_mean_1")
-
-## Generamos el segundo gráfico
-q2 <- ggplot(post_dist, aes(.iteration, b_emg_mean_1, col = as.factor(.chain))) +
-  geom_line() +
-  labs(x = "Iterations",
-       y = "Estimated linear effect of\nEMG on hamstring Torque",
-       col = "Chain") +
-  geom_ysidehistogram(aes(fill = as.factor(.chain)), bins = 100,
-                      show.legend = FALSE) +
-  geom_hline(yintercept = ribbon_data[,"Estimate"]) +
-  geom_hline(yintercept = ribbon_data[,c("Q2.5","Q97.5")], lty = 2) +
-  scale_x_continuous(expand = c(0,0)) +
-  scale_y_continuous(n.breaks = 6) +
-  scale_ysidex_continuous(expand = c(0,0), breaks = NULL, labels = NULL) +
-  scale_color_brewer(aesthetics = c("colour", "fill")) +
-  see::theme_modern() +
-  theme(ggside.panel.scale = 1/3)
-
-print(q2)
-
-## Tercer gráfico ----
-
-## Preparamos los datos a usar
-plot_chains_data <- post_dist[, list(
-    emg_mean = cumulative_emg_mean_1,
-    iter = .iteration,
-    chain = as.factor(.chain)
-  )]
-
-## Estimación del valor central del parámetro modelado
-estimated_value <- bayestestR::point_estimate(model_2, centrality = "median", parameters = "emg")[[2]]
-
-q3 <- ggplot(plot_chains_data, aes(iter, emg_mean)) +
-  geom_line(aes(col = chain)) +
-  labs(x = "Iterations", y = "Estimated linear effect of\nEMG on hamstring Torque",
-       col = "Chain") +
-  scale_color_brewer() +
-  scale_x_log10() +
-  geom_hline(yintercept = estimated_value, lty = 2) +
-  see::theme_modern()
-
-print(q3)
-
-## Unimos los gráficos en uno solo ----
-
-q_plots <- (guide_area() / ((q1 + q3) / q2)) +
-  plot_layout(guides = "collect", heights = c(1,9)) &
-  theme(legend.position = "top")
-
-(p_plots | q_plots) + plot_layout(guides = "collect")
-
-plots <- (guide_area() / (((p1 + p3) / p2) | ((q1 + q3) / q2)))
-
-plots <- plots +
-  plot_layout(guides = "collect", heights = c(1,9)) +
-  plot_annotation(tag_levels = list(c("A"), "1")) &
-  theme(legend.position = "top")
-
-pdf("")
-
+pp_check(model_2, ndraws = 200)  # shows dens_overlay plot by default
+pp_check(model_2, type = "error_hist", ndraws = 12)
+pp_check(model_2, type = "scatter_avg")
+pp_check(model_2, type = "stat_2d")
+pp_check(model_2, type = "loo_ribbon")
+pp_check(model_2, type = "stat_freqpoly", ndraws = 1e4, binwidth = 2)
--- manuscript/figures/torque_&_emg.R
+++ manuscript/figures/torque_&_emg.R
@@ -20,6 +20,11 @@ library(patchwork) ## Para unir los gráficos en uno solo
 data("cyclist")
 
 ## Cargamos los modelos ajustados
+if (any(!file.exists("R/bda/bm/isquios_torque_&_emg.RDS",
+                     "R/bda/bm/quad_torque_&_emg.RDS"))) {
+  stop("Uno o más modelos no han sido generados")
+}
+
 isqt_mod <- readRDS("R/bda/bm/isquios_torque_&_emg.RDS")
 quad_mod <- readRDS("R/bda/bm/quad_torque_&_emg.RDS")
 
@@ -52,92 +57,140 @@ quad_pred <- data.frame(
   emg_mean_1 = seq(from = min(cyclist$emg_mean_1),
                    to = max(cyclist$emg_mean_1),
                    length.out = 10),
-  sd_torque_quad = median(quad_data$sd_torque_quad)
-)
+  sd_torque_quad = median(quad_data$sd_torque_quad))
 
 quad_pred <- predict(
-  object = model,
+  object = quad_mod,
   newdata = quad_pred,
-  probs = c(0.025, .1, .25, .75, .9, .975),
+  probs = c(.005, .025, .1, .25, .75, .9, .975, .995),
   robust = TRUE,
-  cores = 5
-) |>
+  cores = 5) |>
   cbind(quad_pred) |>
   as.data.table()
 
 ## Isquiotibiales
+isqt_pred <- data.frame(
+  emg_mean_1 = seq(from = min(cyclist$emg_mean_1),
+                   to = max(cyclist$emg_mean_1),
+                   length.out = 10),
+  sd_torque_isquio = median(isqt_data$sd_torque_isquio))
 
+isqt_pred <- predict(
+  object = isqt_mod,
+  newdata = isqt_pred,
+  probs = c(.005, .025, .1, .25, .75, .9, .975, .995),
+  robust = TRUE,
+  cores = 5) |>
+  cbind(isqt_pred) |>
+  as.data.table()
 
 # -------------------------------------------------------------------------
 
+scatter_plot = function(pred_data, raw_data, muscle = NULL) {
+  if (is.null(muscle)) {
+    stop("`muscle` must be one of \"quad\" or \"isquio\"")
+  }
+  if (grepl("quad|cuad", muscle, TRUE)) {
+    ylab = "quadriceps"; point_var = "mean_torque_quad"
+  } else if (grepl("hams|isquio", muscle, TRUE)) {
+    ylab = "hamstrings"; point_var = "mean_torque_isquio"
+  } else stop("`muscle` must be one of \"quad\" or \"isquio\"")
 
+  ggplot(pred_data, aes(emg_mean_1, Estimate)) +
+    geom_ribbon(aes(ymin = Q0.5, ymax = Q99.5, fill = "99%")) +
+    geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5, fill = "95%")) +
+    geom_ribbon(aes(ymin = Q10, ymax = Q90, fill = "80%")) +
+    geom_ribbon(aes(ymin = Q25, ymax = Q75, fill = "50%")) +
+    scale_fill_manual(values = `names<-`(RColorBrewer::brewer.pal(4, "Blues"), c("99%", "95%", "80%", "50%"))) +
+    geom_line() +
+    geom_point(data = raw_data, aes(emg_mean_1, .data[[point_var]])) +
+    #scale_y_continuous(n.breaks = 6, limits = c(0, NA)) +
+    scale_x_continuous(#limits = c(0, NA),
+                       labels = function(x) scales::percent(x/100),
+                       n.breaks = 4) +
+    labs(x = "Mean EMG [%]", y = paste("Mean", ylab, "torque [Nm]"), fill = "CI") +
+    see::theme_modern()
+}
 
-## Generamos el primer gráfico
-p1 <- ggplot(pred_data, aes(emg_mean_1, Estimate)) +
-  geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5, fill = "95%")) +
-  geom_ribbon(aes(ymin = Q10, ymax = Q90, fill = "80%")) +
-  geom_ribbon(aes(ymin = Q25, ymax = Q75, fill = "50%")) +
-  scale_fill_manual(values = `names<-`(RColorBrewer::brewer.pal(3, "Blues"), c("95%", "80%", "50%"))) +
-  geom_line() +
-  geom_point(data = quad_iso_data, aes(emg_mean_1, mean_torque_quad)) +
-  scale_y_continuous(n.breaks = 6, limits = c(0, NA)) +
-  scale_x_continuous(limits = c(0, NA)) +
-  labs(x = "Mean EMG", y = "Mean quadriceps torque", fill = "CI") +
-  see::theme_modern()
-
-print(p1)
+p1 <- scatter_plot(quad_pred, quad_data, "quad")
+q1 <- scatter_plot(isqt_pred, isqt_data, "isquio")
 
 ## Segundo gráfico -----
 
-## Obtenemos los muestreos de las simulaciones de las Cadenas de Markov
-post_dist <- tidy_draws(model) |>
-  as.data.table()
+trace_plot = function(model, muscle = NULL) {
+  if (is.null(muscle)) {
+    stop("`muscle` must be one of \"quad\" or \"isquio\"")
+  }
+  ylab <- ifelse(test = grepl("quad|cuad", muscle, TRUE),
+                 yes = "quadriceps", no = "hamstrings")
 
-## Calculamos la media acumulada por cadena para ilustrar la convergencia a la
-## solución del modelo
-post_dist[j = cumulative_emg_mean_1 := cumsum(b_emg_mean_1) / seq_along(b_emg_mean_1),
-          by = list(.chain)]
+  post_dist <- as.data.table(x = tidy_draws(model))
+  hline_data <- posterior_summary(x = model,
+                                  variable = "b_emg_mean_1")
 
-ribbon_data <- posterior_summary(model, variable = "b_emg_mean_1")
+  ggplot(post_dist, aes(.iteration, b_emg_mean_1, col = as.factor(.chain))) +
+    geom_line() +
+    labs(x = "Iterations (thousands)",
+         y = paste("Estimated linear effect of\nEMG on", ylab, "torque"),
+         col = "Chain") +
+    geom_ysidehistogram(aes(fill = as.factor(.chain)), bins = 100,
+                        show.legend = FALSE) +
+    geom_hline(yintercept = hline_data[,"Estimate"]) +
+    geom_hline(yintercept = hline_data[,c("Q2.5","Q97.5")], lty = 2) +
+    scale_x_continuous(expand = c(0,0),
+                       labels = function(i) i/1000) +
+    scale_y_continuous(n.breaks = 6) +
+    scale_ysidex_continuous(expand = c(0,0), breaks = NULL, labels = NULL) +
+    scale_color_brewer(aesthetics = c("colour", "fill")) +
+    see::theme_modern() +
+    theme(ggside.panel.scale = 1/3)
+}
 
-## Generamos el segundo gráfico
-p2 <- ggplot(post_dist, aes(.iteration, b_emg_mean_1, col = as.factor(.chain))) +
-  geom_line() +
-  labs(x = "Iterations",
-       y = "Estimated linear effect of\nEMG on quadriceps Torque",
-       col = "Chain") +
-  geom_ysidehistogram(aes(fill = as.factor(.chain)), bins = 100,
-                      show.legend = FALSE) +
-  geom_hline(yintercept = ribbon_data[,"Estimate"]) +
-  geom_hline(yintercept = ribbon_data[,c("Q2.5","Q97.5")], lty = 2) +
-  scale_x_continuous(expand = c(0,0)) +
-  scale_y_continuous(n.breaks = 6) +
-  scale_ysidex_continuous(expand = c(0,0), breaks = NULL, labels = NULL) +
-  scale_color_brewer(aesthetics = c("colour", "fill")) +
-  see::theme_modern() +
-  theme(ggside.panel.scale = 1/3)
-
-print(p2)
+p2 <- trace_plot(quad_mod, "quad")
+q2 <- trace_plot(isqt_mod, "isquios")
 
 ## Tercer gráfico ----
 
-## Preparamos los datos a usar
-plot_chains_data <- post_dist[, list(
-  emg_mean = cumulative_emg_mean_1,
-  iter = .iteration,
-  chain = as.factor(.chain)
-)]
+convergence_plots = function(model, muscle) {
+  if (is.null(muscle)) {
+    stop("`muscle` must be one of \"quad\" or \"isquio\"")
+  }
+  ylab <- ifelse(test = grepl("quad|cuad", muscle, TRUE),
+                 yes = "quadriceps", no = "hamstrings")
 
-## Estimación del valor central del parámetro modelado
-estimated_value <- bayestestR::point_estimate(model, centrality = "median", parameters = "emg")[[2]]
+  post_dist <- as.data.table(x = tidy_draws(model))
+  post_dist[j = cumulative_emg_mean_1 := cumsum(b_emg_mean_1) / seq_along(b_emg_mean_1),
+            by = list(.chain)]
+  post_dist <- post_dist[, list(
+    emg_mean = cumulative_emg_mean_1,
+    iter = .iteration,
+    chain = as.factor(.chain)
+  )]
 
-p3 <- ggplot(plot_chains_data, aes(iter, emg_mean)) +
-  geom_line(aes(col = chain)) +
-  labs(x = "Iterations", y = "Estimated linear effect of\nEMG on quadriceps Torque",
-       col = "Chain") +
-  scale_color_brewer() +
-  scale_x_log10() +
-  geom_hline(yintercept = estimated_value, lty = 2) +
-  see::theme_modern()
+  hline_data <- posterior_summary(x = model,
+                                  variable = "b_emg_mean_1")
 
-print(p3)
+  ggplot(post_dist, aes(iter, emg_mean)) +
+    geom_line(aes(col = chain)) +
+    labs(x = "Iterations (log scale)", y = paste("Estimated linear effect of\nEMG on", ylab, "torque"),
+         col = "Chain") +
+    scale_color_brewer() +
+    scale_x_continuous(trans = "log10",
+                       breaks = scales::trans_breaks("log10", function(x) 10^x),
+                       labels = scales::trans_format("log10", scales::math_format(10^.x))) +
+    geom_hline(yintercept = hline_data[,"Estimate"], lty = 2) +
+    see::theme_modern()
+}
+
+p3 <- convergence_plots(quad_mod, "quad")
+q3 <- convergence_plots(isqt_mod, "isquios")
+
+plots <- (guide_area() / (((p1 + p3) / p2) | ((q1 + q3) / q2)))
+
+plots <- plots +
+  plot_layout(guides = "collect", heights = c(1,9)) +
+  plot_annotation(tag_levels = list(c("A"), "1")) &
+  theme(legend.position = "top")
+
+ggsave("manuscript/figures/torque_&_emg.pdf", plots, "pdf", width = 12, height = 8, units = "in")
+ggsave("manuscript/figures/torque_&_emg.jpeg", plots, "jpeg", width = 12, height = 8, units = "in", dpi = 300)
