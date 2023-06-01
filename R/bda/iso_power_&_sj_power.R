5aa4ed6a667a029519c29c85903ada82800688c1
Matías Castillo-Aguilar <matcasti@umag.cl>
2023-06-01 20:22:11 -0300

    still working on another model but first emg and iso torque are ready

---
+++ R/bda/iso_power_&_sj_power.R
@@ -0,0 +1,111 @@
+
+# Preparamos espacio de trabajo -------------------------------------------
+
+## Cargamos paquetes ----
+
+## Manipulación de datos
+library(data.table)
+
+## Modelos bayesianos
+library(brms) ## Paquete principal
+
+## Cargamos los datos
+data("cyclist")
+
+########################################################
+############## Estimación con iso_quad #################
+########################################################
+
+# Ajustamos los modelos ---------------------------------------------------
+
+quad_data <- cyclist[i = !is.na(iso_power_per_kg_quad_der_norm),
+                     j = list(
+                       mean_iso_power_quad = mean(c(iso_power_per_kg_quad_der_norm, iso_power_per_kg_quad_izq_norm)),
+                       sd_iso_power_quad = sd(c(iso_power_per_kg_quad_der_norm, iso_power_per_kg_quad_izq_norm)),
+                       jump_sj_power_mean
+                     ),
+                     by = sujetos]
+
+quad_model <- brm(
+  ## Especificamos el modelo
+  formula = mean_iso_power_quad | se(sd_iso_power_quad) ~ jump_sj_power_mean,
+
+  ## Escogemos la distribución a priori de los coeficientes
+  prior = prior(prior = student_t(3, 0, 10), class = b),
+
+  ## Datos a usar
+  data = quad_data,
+
+  ## Familia de distribución a usar
+  family = gaussian(),
+
+  ## Hiperparámetros del modelo
+  chains = 5L, iter = 12e3, warmup = 2e3,
+  control = list(max_treedepth = 100, adapt_delta = .99),
+  cores = 5L, backend = "cmdstanr",
+  save_pars = save_pars(all = TRUE),
+  seed = 1234
+)
+
+saveRDS(quad_model, file = "R/bda/bm/quad_iso_torque_&_sj_power.RDS")
+
+## Valoración del ajuste del modelo
+# pp_check(quad_model, ndraws = 200)  # shows dens_overlay plot by default
+# pp_check(quad_model, type = "error_hist", ndraws = 12)
+# pp_check(quad_model, type = "scatter_avg")
+# pp_check(quad_model, type = "stat_2d")
+# pp_check(quad_model, type = "loo_ribbon")
+# pp_check(quad_model, type = "stat_freqpoly", ndraws = 1e4, binwidth = 2)
+#
+# loo(quad_model, reloo = TRUE)
+
+
+########################################################
+############# Estimación con iso_isquio ################
+########################################################
+
+
+# Ajustamos los modelos ---------------------------------------------------
+
+isqt_data <- cyclist[i = !is.na(iso_mean_torque_isquio_der_raw),
+                     j = list(
+                       mean_torque_isquio = mean(x = c(iso_mean_torque_isquio_der_raw, iso_mean_torque_isquio_izq_raw)),
+                       sd_torque_isquio = mean(x = c(iso_sd_torque_isquio_der_raw, iso_sd_torque_isquio_izq_raw)),
+                       emg_mean_1
+                     ),
+                     by = sujetos]
+
+## Vemos los priors por defecto
+get_prior(formula = mean_torque_isquio | se(sd_torque_isquio) ~ emg_mean_1, data = isqt_data)
+
+isqt_model <- brm(
+  ## Especificamos el modelo
+  formula = mean_torque_isquio | se(sd_torque_isquio) ~ emg_mean_1,
+
+  ## Escogemos la distribución a priori de los coeficientes
+  prior = prior(prior = student_t(3, 0, 5), class = b),
+
+  ## Datos a usar
+  data = isqt_data,
+
+  ## Familia de distribución a usar
+  family = gaussian(),
+
+  ## Hiperparámetros del modelo
+  chains = 5L, iter = 12e3, warmup = 2e3,
+  cores = 5L, backend = "cmdstanr",
+  save_pars = save_pars(all = TRUE),
+  seed = 1234
+)
+
+saveRDS(isqt_model, file = "R/bda/bm/isquios_iso_torque_&_sj_power.RDS")
+
+## Valoración del ajuste del modelo
+# pp_check(isqt_model, ndraws = 200)  # shows dens_overlay plot by default
+# pp_check(isqt_model, type = "error_hist", ndraws = 12)
+# pp_check(isqt_model, type = "scatter_avg")
+# pp_check(isqt_model, type = "stat_2d")
+# pp_check(isqt_model, type = "loo_ribbon")
+# pp_check(isqt_model, type = "stat_freqpoly", ndraws = 1e4, binwidth = 2)
+#
+# loo(isqt_model, reloo = TRUE)
