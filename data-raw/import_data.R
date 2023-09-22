b5cf778b2d6817117722ab0b4b01fa67dbe6dffb
Matías Castillo <matcasti@umag.cl>
2023-09-22 15:18:34 -0300

    data processing script done

--- data-raw/import_data.R
+++ data-raw/import_data.R
@@ -1,49 +1,17 @@
 library(readxl)
 library(data.table)
 
-iso <- as.data.table(
+cyclist <- as.data.table(
   x = read_excel(
     path = "data-raw/data-raw.xlsx",
-    sheet = "ISOCINETICA",
-    range = "A4:AU14",
-    col_names = readLines("data-raw/misc/iso_titles.txt")
+    sheet = "con salto v2",
+    range = "A3:AM13",
+    col_names = readLines("data-raw/misc/colnames.txt")
   )
 )
 
-salto <- as.data.table(
-  x = read_excel(
-    path = "data-raw/data-raw.xlsx",
-    sheet = "SALTO",
-    range = "A4:X14",
-    col_names = readLines("data-raw/misc/jump_titles.txt")
-  )
-)
+str(cyclist)
 
-emg <- as.data.table(
-  x = read_excel(
-    path = "data-raw/data-raw.xlsx",
-    sheet = "EMG",
-    range = "A4:J14",
-    col_names = readLines("data-raw/misc/emg_titles.txt")
-  )
-)
-
-emg[sujetos == "Fernando Mallada", sujetos := "Fernando Mellada"]
-
-if (all(identical(iso$sujetos, salto$sujetos),
-    identical(emg$sujetos, salto$sujetos),
-    identical(iso$sujetos, emg$sujetos))) {
-  cyclist <- merge(merge(iso, salto, by = "sujetos"), emg, by = "sujetos")
-  rm(emg, iso, salto)
-} else stop("Los nombres no coinciden entre almenos un conjunto de datos.\n¡Comprobarlo!")
-
-
-cyclist[j = `:=`(
-          mean_torque_quad = median(x = c(iso_mean_torque_quad_der_raw, iso_mean_torque_quad_izq_raw)),
-          sd_torque_quad = median(x = c(iso_sd_torque_quad_der_raw, iso_sd_torque_quad_izq_raw)),
-          mean_torque_isquio = mean(x = c(iso_mean_torque_isquio_der_raw, iso_mean_torque_isquio_izq_raw)),
-          sd_torque_isquio = mean(x = c(iso_sd_torque_isquio_der_raw, iso_sd_torque_isquio_izq_raw))),
-        by = sujetos][]
 
 save(cyclist, file = "data/cyclist.RData")
 save(cyclist, file = "docs/manuscript/misc/cyclist.RData")
